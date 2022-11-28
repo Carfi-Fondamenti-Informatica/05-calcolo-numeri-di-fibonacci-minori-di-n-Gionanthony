666c0df411a656cc5a92c25bffeea2a6f553c620
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-11-28 09:04:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+
+    int n;
+    int p=0;
+    int s=1;
+    int i=1;
+    cin >> n;
+    cout << s << endl;
+    while (i++ <=n){
+        cout<<p+s<<endl;
+        if(p<s){
+            p+=s;
+        }else{
+            s+=p;
+        }
+        if ((p+s)>n)
+            break;
+    }
+
+    return 0;
 }
